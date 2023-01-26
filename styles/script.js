5ccfa97d0b33ae56d1cd001febf99be6b0969c18
joelle78 <96306220+joelle78@users.noreply.github.com>
2023-01-26 01:23:46 +0100

    implementation

--- styles/script.js
+++ styles/script.js
@@ -1,3 +1,46 @@
+//Cube 19 senior brand designers
+document.getElementById('build-button').addEventListener("click",
+    function () {
+        document.getElementById('buildCube19').style.animation = "slideIn 2s forwards";
+    });
+
+//Cube 16 senior UX designers
+document.getElementById('build-button').addEventListener("click",
+    function () {
+        document.getElementById('buildCube16').style.animation = "slideIn-2 2s forwards";
+        document.getElementById('buildCube16').style.animationDelay = "1s";
+    });
+
+//Cube 13 developers
+document.getElementById('build-button').addEventListener("click",
+    function () {
+        document.getElementById('buildCube13').style.animation = "slideIn-3 2s forwards";
+        document.getElementById('buildCube13').style.animationDelay = "1.8s";
+    });
+
+//Cube 10 UX'ers
+document.getElementById('build-button').addEventListener("click",
+    function () {
+        document.getElementById('buildCube10').style.animation = "slideIn-4 2s forwards";
+        document.getElementById('buildCube10').style.animationDelay = "2.6s";
+    });
+
+//Cube 7 junior designer
+document.getElementById('build-button').addEventListener("click",
+    function () {
+        document.getElementById('buildCube7').style.animation = "slideIn-5 2s forwards";
+        document.getElementById('buildCube7').style.animationDelay = "3.2s";
+    });
+
+//Cube 4 open application
+document.getElementById('build-button').addEventListener("click",
+    function () {
+        document.getElementById('buildCube4').style.animation = "slideIn-6 2s forwards";
+        document.getElementById('buildCube4').style.animationDelay = "4s";
+    });
+
+
+// navbar
 window.addEventListener('DOMContentLoaded', () => {
 
     const observer = new IntersectionObserver(entries => {
